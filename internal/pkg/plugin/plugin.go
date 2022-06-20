9d9bbc4c4ddc2ada1f2cc12dc089bb0fec63d721
Itay Donanhirsh <itay@bazoo.org>
2022-06-19 23:05:53 -0700

    creds as value

--- internal/pkg/plugin/plugin.go
+++ internal/pkg/plugin/plugin.go
@@ -16,11 +16,30 @@ import (
 //       specify the configuration. This is for demo purposes only.
 var awsConfig aws.Config
 
+var credentials *apivalues.Value
+
 func init() {
 	var err error
 	if awsConfig, err = config.LoadDefaultConfig(context.Background()); err != nil {
 		panic(err)
 	}
+
+	creds, err := awsConfig.Credentials.Retrieve(context.Background())
+	if err != nil {
+		panic(fmt.Errorf("credentials retreive: %w", err))
+	}
+
+	credentials = apivalues.Struct(
+		apivalues.Symbol("aws_credentials"),
+		map[string]*apivalues.Value{
+			"access_key_id":     apivalues.String(creds.AccessKeyID),
+			"secret_access_key": apivalues.String(creds.SecretAccessKey),
+			"session_token":     apivalues.String(creds.SessionToken),
+			"source":            apivalues.String(creds.Source),
+			"can_expire":        apivalues.Boolean(creds.CanExpire),
+			"expires":           apivalues.Time(creds.Expires),
+		},
+	)
 }
 
 var (
@@ -30,38 +49,8 @@ var (
 			Doc: "AWS SDK Plugin",
 			Members: map[string]*pluginimpl.PluginMember{
 				"ec2":                importService("EC2", ec2.NewFromConfig),
-				"secret_credentials": credentials,
+				"secret_credentials": pluginimpl.NewValueMember("AWS Credentials", credentials),
 			},
 		}
 	}
-
-	credentials = pluginimpl.NewSimpleMethodMember(
-		"retreive AWS credentials",
-		func(
-			ctx context.Context,
-			args []*apivalues.Value,
-			kwargs map[string]*apivalues.Value,
-		) (*apivalues.Value, error) {
-			if err := pluginimpl.UnpackArgs(args, kwargs); err != nil {
-				return nil, err
-			}
-
-			creds, err := awsConfig.Credentials.Retrieve(ctx)
-			if err != nil {
-				return nil, fmt.Errorf("retreive: %w", err)
-			}
-
-			return apivalues.Struct(
-				apivalues.Symbol("aws_credentials"),
-				map[string]*apivalues.Value{
-					"access_key_id":     apivalues.String(creds.AccessKeyID),
-					"secret_access_key": apivalues.String(creds.SecretAccessKey),
-					"session_token":     apivalues.String(creds.SessionToken),
-					"source":            apivalues.String(creds.Source),
-					"can_expire":        apivalues.Boolean(creds.CanExpire),
-					"expires":           apivalues.Time(creds.Expires),
-				},
-			), nil
-		},
-	)
 )
