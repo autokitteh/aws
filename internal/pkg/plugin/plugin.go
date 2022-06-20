de931082d1a497d540b09b9c27f3c858362c35a8
Itay Donanhirsh <itay@bazoo.org>
2022-06-19 22:59:59 -0700

    add credentials retreive

--- internal/pkg/plugin/plugin.go
+++ internal/pkg/plugin/plugin.go
@@ -2,11 +2,13 @@ package plugin
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 
+	"go.autokitteh.dev/sdk/api/apivalues"
 	"go.autokitteh.dev/sdk/pluginimpl"
 )
 
@@ -27,8 +29,39 @@ var (
 			ID:  "aws",
 			Doc: "AWS SDK Plugin",
 			Members: map[string]*pluginimpl.PluginMember{
-				"ec2": importService("EC2", ec2.NewFromConfig),
+				"ec2":                importService("EC2", ec2.NewFromConfig),
+				"secret_credentials": credentials,
 			},
 		}
 	}
+
+	credentials = pluginimpl.NewSimpleMethodMember(
+		"retreive AWS credentials",
+		func(
+			ctx context.Context,
+			args []*apivalues.Value,
+			kwargs map[string]*apivalues.Value,
+		) (*apivalues.Value, error) {
+			if err := pluginimpl.UnpackArgs(args, kwargs); err != nil {
+				return nil, err
+			}
+
+			creds, err := awsConfig.Credentials.Retrieve(ctx)
+			if err != nil {
+				return nil, fmt.Errorf("retreive: %w", err)
+			}
+
+			return apivalues.Struct(
+				apivalues.Symbol("aws_credentials"),
+				map[string]*apivalues.Value{
+					"AccessKeyID":     apivalues.String(creds.AccessKeyID),
+					"SecretAccessKey": apivalues.String(creds.SecretAccessKey),
+					"SessionToken":    apivalues.String(creds.SessionToken),
+					"Source":          apivalues.String(creds.Source),
+					"CanExpire":       apivalues.Boolean(creds.CanExpire),
+					"Expires":         apivalues.Time(creds.Expires),
+				},
+			), nil
+		},
+	)
 )
